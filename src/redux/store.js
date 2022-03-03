f1d105b958881e266a47132df83146366399066f
MarkLurton <marklurton@gmail.com>
2022-03-03 15:07:34 -0600

    change middlewares to not log in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,7 @@ import rootReducer from './root-reducer';
 
 const middlewares = [];
 
-if (ProcessingInstruction.env.NODE_EN == 'development') {
+if (ProcessingInstruction.env.NODE_ENV == 'development') {
     middlewares.push(logger);
 }
 
